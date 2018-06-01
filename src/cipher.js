019f34db08a46cf5a721e488ad8d5f31d7dd8a7c
jimenaacuna <jimena.a.saavedra@gmail.com>
2018-06-01 15:26:17 -0400

    correcciones 2

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,36 @@
+
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+
+  encode: (text,offset) => {
+
+    text = text.toUpperCase();
+   
+  let cipher = "";
+
+  for(let i = 0; i < text.length; i++) { 
+    const ASCII = text.charCodeAt(i);
+    let travelposition = (ASCII - 65 + offset) % 26 + 65;
+    const newASCII = String.fromCharCode(travelposition);
+    cipher += newASCII;
+  }
+  return cipher;
+
+  
+
   },
-  decode: () => {
-    /* Acá va tu código */
+  decode: (text,offset) => {
+
+    text = text.toUpperCase();
+
+    let decipher = "";
+
+    for(let a = 0; a < text.length; a++) {
+      const ASCII = text.charCodeAt(a);
+      let travelposition = (ASCII + 65 - offset) % 26 + 65;
+      const newASCII = String.fromCharCode(travelposition);
+      decipher += newASCII;
+    }
+    return decipher;
+    
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 /* Acá va tu código */
 
+document.getElementById('encode').addEventListener('click',function(){
+    let string2 = document.getElementById("boxCipher");
+    let num = document.getElementById("positions");
+    document.getElementById("boxMensaje").innerHTML = window.cipher.encode(string2.value,parseInt(num.value));
+})
+    
+
+
+document.getElementById('decode').addEventListener('click',function(){
+    let string2 = document.getElementById("boxCipher");
+    let num = document.getElementById("positions");
+    document.getElementById("boxMensaje").innerHTML = window.cipher.decode(string2.value,parseInt(num.value));
+})
 
 
 
+
+
+    /* document.getElementById("boxMensaje").innerHTML = window.cipher.decode(string2.value,parseInt(offset2.value)); */
+
+/* function reiniciar() {
+    document.getElementById("boxMensaje").reset;
+} */
 
 
 
